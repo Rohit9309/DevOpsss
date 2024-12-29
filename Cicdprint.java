8f59448578a4ddff35bf33dab2ed3c50e79f65f9
Rohit Gavali <rohit.gavali_24uds@sanjivani.edu.in>
2024-12-29 20:43:55 +0530

    Updated message

--- Cicdprint.java
+++ Cicdprint.java
@@ -2,7 +2,7 @@ public class Cicdprint
 {
     public static void main(String args[])
     {
-        String print="Welcome to the CI/CD Pipeline Practical!";
+        String print="Welcome to the CI/CD Pipeline Practical! to have to perform a task like GitHub & jenkins for automation to project..";
         System.out.println(print);
     }
 }
