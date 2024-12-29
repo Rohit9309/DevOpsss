dfe95166e8c4a203569d8f79340288fe24114653
Rohit Gavali <rohit.gavali_24uds@sanjivani.edu.in>
2024-12-29 20:08:23 +0530

    Message are ready to shows in github

---
+++ Cicdprint.java
@@ -0,0 +1,8 @@
+public class Cicdprint
+{
+    public static void main(String args[])
+    {
+        String print="Welcome to the CI/CD Pipeline Practical!";
+        System.out.println(print);
+    }
+}
